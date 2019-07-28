14a63e1bf3f76a03935efd4ccc81b0acfe56ecc4
Daniel Matteson <dmatt@disqus.com>
2019-07-28 15:07:56 -0700

    Add puzzle challenge class and implement another scene

--- python/oop_game.py
+++ python/oop_game.py
@@ -1,6 +1,50 @@
 from sys import exit
+import random
 from random import randint
+from urllib import urlopen
 from six.moves import input as input
+
+class Challenge(object):
+    """
+    use challenge_user() to provide a challenge that changes each instance, can
+    be type guess passoword or close_airlock, and both will return 'success' or 'failure'
+    """
+    CHALLENGE_TYPES = ['guess_password', 'close_airlock']
+    WORD_URL = "http://learncodethehardway.org/words.txt"
+    WORDS = []
+
+    def __init__(self, type='guess_password'):
+        self.type = type
+        self.guesses_left = 3
+        self.word = self.get_random_word()
+        self.obfuscated_word = self.word.replace(self.word[randint(0, len(self.word)-1)], '*')
+        self.completed = False
+
+    def get_random_word(self):
+        try:
+            # load up the words from the website
+            for word in urlopen(self.WORD_URL).readlines():
+                self.WORDS.append(word.strip().decode("utf-8"))
+            return random.sample(self.WORDS, 1)[0]
+        except Exception as e:
+            print('Could not open word URL because: %s' % e)
+
+    def prompt_guess(self):
+        word = self.word
+        hint = 'The password is missing letters for some strange reason, guess the full password: '
+        guesses_left = 'You have %s guesses left!' % self.guesses_left
+        guess = input('%s %s %s \n >' % (guesses_left, hint, self.obfuscated_word))
+        self.guesses_left -= 1
+        return guess
+
+    def challenge_user(self):
+        answer = self.prompt_guess()
+        if answer == self.word:
+            import pdb; pdb.set_trace()
+            self.completed = True
+            return 'success'
+        else:
+            return 'failure'
 
 class Scene(object):
     def enter(self):
@@ -15,7 +59,7 @@ class Engine(object):
         current_scene = self.scene_map.opening_scene()
         while True:
             print("\n - ")
-            # Jump into current scene and stored returned result as `result_string`
+            # Jump into current scene and store returned result as `result_string`
             result_string = current_scene.enter()
             # Progress forward by overriding the new current_scene for next iteration of loop 
             current_scene = self.scene_map.next_scene(result_string)            
@@ -51,21 +95,44 @@ class CentralCorridor(Scene):
         print('after getting into Escape pod. U are now running down corridor to Armory, scary Alien jump out blocking door')
         print('What do?')
 
-        action = input("S for shoot, D for dodge, C for cry >")
+        action = input("T for throw shoe, D for dodge, C for cry >")
 
-        if action.upper() == 'S':
-            print('U try to shoot but if makes Alien angry, not ded. He bite ur face off.')
+        if action.upper() == 'T':
+            print('U try to throw shoe but if makes Alien angry, not ded. He bite ur face off.')
             return 'death'
         elif action.upper() == 'D':
-            print('You jump out of the way and sruvive, keep running to escape pod')
-            return 'escape pod'
+            print('You jump out of the way and sruvive, you head to the Armory to get lots o\' guns')
+            return 'laser_weapon_armory'
         elif action.upper() == 'C':
-            print('U cry baby tears and alien feels bad and helps you to escape pod')
-            return 'escape pod'
+            print('U cry baby tears and alien feels bad, he gives you directions to Armory to get lots o\' guns')
+            return 'laser_weapon_armory'
 
 class LaserWeaponArmory(Scene):
     def enter(self):
-        pass
+        print('You walk inside bright room filled with weapons along all walls')
+        print('And a suitcase conveiniently sits in the middle of the room')
+        print('You see a wall of swords, a wall of lazer machine guns, and a wall of baseball bats')
+        print('What do?')
+
+        action = input("S for get swords, L for lazer guns, B for bomb \n>")
+
+        if action.upper() == 'S':
+            print('The swords are too spikey and don\'t fit in suitcase, takes too long and alien eat you')
+            return 'death'
+        elif action.upper() == 'B':
+            print('you must unlock the bomb first.')
+            bomb_challenge = Challenge()
+            while bomb_challenge.guesses_left > 0 and not bomb_challenge.completed:
+                result = bomb_challenge.challenge_user()
+            if result == 'success':
+                print('Good jobs, you unlocked the bomb, head to the Bridge to blow that sucker up')
+                return 'the_bridge'
+            else:
+                print('Sorry you guessed really bad, intruder detected, bomb go into self-destruct mode now...')
+                return 'death'
+        elif action.upper() == 'L':
+            print('You put a few Lazer guns into your bag and you must run to the bridge to blow it up')
+            return 'the_bridge'
 
 class TheBridge(Scene):
     def enter(self):
@@ -99,3 +166,4 @@ if __name__ == '__main__':
     a_map = Map('central_cooridor')
     a_game = Engine(a_map)
     a_game.play()
+    # print(Challenge('guess_password').prompt_guess())
